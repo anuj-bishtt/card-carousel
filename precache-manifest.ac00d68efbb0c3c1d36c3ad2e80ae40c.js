c03765d55f1d6058fd6894be2645dbbca6f38ecb
anuj-bishtt <anuj.srmcem2310@gmail.com>
2023-04-18 17:37:15 +0530

    Updates

---
+++ precache-manifest.ac00d68efbb0c3c1d36c3ad2e80ae40c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326258dc71154b149b97194d9b833965",
+    "url": "/card-carousel/index.html"
+  },
+  {
+    "revision": "88693bfc0a4e619a8feb",
+    "url": "/card-carousel/static/css/main.f5734a2f.chunk.css"
+  },
+  {
+    "revision": "eaf559e415d3c9a6e5c7",
+    "url": "/card-carousel/static/js/2.556a8dfa.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/card-carousel/static/js/2.556a8dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88693bfc0a4e619a8feb",
+    "url": "/card-carousel/static/js/main.c2804456.chunk.js"
+  },
+  {
+    "revision": "2935e3febb94cd7de1ad",
+    "url": "/card-carousel/static/js/runtime-main.6f77a75c.js"
+  },
+  {
+    "revision": "615f3dd5a1c7b13041dae8163e00e0a1",
+    "url": "/card-carousel/static/media/four-fam.615f3dd5.jpg"
+  }
+]);
